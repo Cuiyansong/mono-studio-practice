7d503b6aadd4256372424665ac86f8de21bb27a1
cuiyansong <yscui@thoughtworks.com>
2015-06-07 00:50:35 +0800

    2. Refactor parking manger

--- Mono_Study/ParkProblem/ParkingBase.cs
+++ Mono_Study/ParkProblem/ParkingBase.cs
@@ -6,18 +6,32 @@ namespace ParkProblem
 {
 	public abstract class ParkingBase
 	{
+		#region Property
+		/// <summary>
+		/// The parklots.
+		/// </summary>
 		protected List<Parklot> parklots = new List<Parklot> ();
-
 		/// <summary>
 		/// Default compare returen a not empty parklot.
 		/// </summary>
 		protected Func<Parklot,Parklot,int> CompareFunc = (x, y) => x.Capacity - 0;
+		#endregion
 
+		#region Method
+		/// <summary>
+		/// Takes the out.
+		/// </summary>
+		/// <returns>The out.</returns>
+		/// <param name="id">Identifier.</param>
 		public virtual Car TakeOut (string id)
 		{  
 			return this.parklots.Find (x => x.ContainsCar (id)).TakeOut (id);
 		}
-
+		/// <summary>
+		/// Parks the in.
+		/// </summary>
+		/// <returns><c>true</c>, if in was parked, <c>false</c> otherwise.</returns>
+		/// <param name="car">Car.</param>
 		public virtual bool ParkIn (Car car)
 		{
 			if (parklots.Count == 0)
@@ -26,11 +40,16 @@ namespace ParkProblem
 			parklots.Sort (CompareFunc.AsCompare ());
 			return	parklots [0].ParkIn (car); 
 		}
-
+		/// <summary>
+		/// Adds the parklots.
+		/// </summary>
+		/// <param name="parks">Parks.</param>
 		public void AddParklots (IEnumerable<Parklot> parks)
 		{
 			this.parklots.AddRange (parks);
 		}
+		#endregion
+
 	}
 }
 
